d594aab1199c857c3b21b3587b8f4d6f51f8bdf6
godianz <godian778@gmail.com>
2023-04-20 01:16:06 +0900

    main
    main

---
+++ js/main.js
@@ -0,0 +1,57 @@
+$(document).ready ( function (){
+    var Time=300;
+    $(".whole").animate({
+            opacity:'1'
+    }, Time, function(){
+        $(".box1").animate({
+            opacity:'1'
+        },Time, function(){
+            $(".box2").animate({
+                opacity:'1'
+            }, Time, function (){
+                $(".box3").animate({
+                    opacity:'1'
+                }, Time, function (){
+                    $(".box4").animate({
+                        opacity:'1'
+                    }, Time, function (){
+                        $(".box5").animate({
+                            opacity:'1'
+                        }, Time, function(){
+                            $(".box6").animate({
+                                opacity:'1'
+                            },Time)
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
+
+var Dropdown = document.getElementsByClassName("Dropdown-Btn");
+var i;
+
+for(i=0; i<Dropdown.length; i++) {
+    Dropdown[i].addEventListener("click",function(){
+        this.classList.toggle("active");
+        var DropdownContent = this.nextElementSibling;
+        if(DropdownContent.style.display === "flex") {
+            DropdownContent.style.display = "none";
+        } else {
+            DropdownContent.style.display = "flex";
+        }
+    });
+}
+
+$(".board1").click(function(){
+    location.href="/companyProject.html"
+})
+
+$(".board2").click(function(){
+    location.href="/TeamProject.html"
+})
+
+$(".board3").click(function(){
+    location.href="/ToyProject.html"
+})
